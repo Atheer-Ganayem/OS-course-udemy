575df2bd895ed6bf32c7951f7452db15cf1e21e5
Atheer-Ganayem <atheer95@gmail.com>
2026-03-09 12:24:23 +0200

    initial disk implementation

--- src/kernel.c
+++ src/kernel.c
@@ -5,6 +5,7 @@
 #include "io/io.h"
 #include "memory/heap/kheap.h"
 #include "memory/paging/paging.h"
+#include "disk/disk.h"
 
 uint16_t* video_mem = 0;
 uint16_t terminal_row = 0, terminal_col = 0;
@@ -65,22 +66,11 @@ void kernel_main() {
   idt_init();
 
   kernel_4gb_chunck = paging_new_4gb(PAGING_IS_WRITEABLE | PAGING_IS_PRESENT | PAGING_ACCESS_FROM_ALL);
-
   paging_swtich(paging_4gb_chunck_get_directory(kernel_4gb_chunck));
-
-  char* ptr = kzalloc(4096);
-  paging_set(paging_4gb_chunck_get_directory(kernel_4gb_chunck), (void*)0x1000, (uint32_t)ptr | PAGING_ACCESS_FROM_ALL | PAGING_IS_PRESENT | PAGING_IS_WRITEABLE);
-  
   enable_paging();
 
-  char* ptr2 = (char*) 0x1000;
-
-  ptr2[0] = 'A';
-  ptr2[1] = 'B';
-
-  print(ptr2);
-  print(ptr);
-
+  char buf[512];
+  disk_read_sector(0, 1, buf);
 
   enable_interrupts();
 }
