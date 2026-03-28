7e48ab3b5441c96a9279130624abd56d6bd6fea4
Atheer-Ganayem <atheer95@gmail.com>
2026-03-29 00:17:16 +0300

    TSS segment

--- src/kernel.c
+++ src/kernel.c
@@ -1,5 +1,5 @@
 #include <kernel.h>
-#include <stdint.h>
+#include "stdint.h"
 #include <stddef.h>
 #include "idt/idt.h"
 #include "io/io.h"
@@ -11,6 +11,7 @@
 #include "gdt/gdt.h"
 #include "config.h"
 #include "memory/memory.h"
+#include "task/tss.h"
 
 uint16_t* video_mem = 0;
 uint16_t terminal_row = 0, terminal_col = 0;
@@ -61,11 +62,16 @@ void panic(const char* msg) {
   while(1){}
 }
 
+struct tss tss;
+
 struct gdt gdt_real [PEACHOS_TOTAL_GDT_SEGMENTS];
 struct gdt_structured gdt_structured[PEACHOS_TOTAL_GDT_SEGMENTS] = {
-  {.base = 0x00, .limit = 0x00, .type = 0x00},          // NULL segment
-  {.base = 0x00, .limit = 0xFFFFFFFF, .type = 0x9A},    // Kernel code segment
-  {.base = 0x00, .limit = 0xFFFFFFFF, .type = 0x92},    // Kernel data segment
+  {.base = 0x00, .limit = 0x00, .type = 0x00},                  // NULL segment
+  {.base = 0x00, .limit = 0xFFFFFFFF, .type = 0x9A},            // Kernel code segment
+  {.base = 0x00, .limit = 0xFFFFFFFF, .type = 0x92},            // Kernel data segment
+  {.base = 0x00, .limit = 0xFFFFFFFF, .type = 0xF8},            // User data segment
+  {.base = 0x00, .limit = 0xFFFFFFFF, .type = 0xF2},            // User data segment
+  {.base = (uint32_t)(&tss), .limit = sizeof(tss), .type = 0xE9}, // TSS segment
 };
 
 void kernel_main() {
@@ -83,6 +89,12 @@ void kernel_main() {
   disk_search_and_init();
 
   idt_init();
+
+  // setup tss
+  memset(&tss, 0x00, sizeof(tss));
+  tss.esp0 = 0x600000;
+  tss.ss0 = KERNEL_DATA_SELECTOR;
+  tss_load(0x28);
 
   kernel_4gb_chunk = paging_new_4gb(PAGING_IS_WRITEABLE | PAGING_IS_PRESENT | PAGING_ACCESS_FROM_ALL);
   paging_switch(paging_4gb_chunk_get_directory(kernel_4gb_chunk));
